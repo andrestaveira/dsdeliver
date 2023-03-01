d3db520ab5fbfe53bffcb5bd8eaaba6f91f1db53
André Taveira <andresvtaveira@gmail.com>
2023-03-01 00:16:01 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+        </footer>
+    )
+}
+
+export default Footer;
