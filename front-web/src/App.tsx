82f3c4a0d72a31eab2b1fb2f085beb996207a60e
André Taveira <andresvtaveira@gmail.com>
2023-03-01 23:19:13 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,14 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Router from './Router';
 
 function App() {
   return (
+    <>
       <Router />
+      <ToastContainer />
+    </>
   );
 }
 
