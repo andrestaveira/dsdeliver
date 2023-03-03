ad2013d63947f7d0b8ec8f0baf2780518ca240ca
André Taveira <andresvtaveira@gmail.com>
2023-03-02 22:35:13 -0300

    Fix total

--- backend/src/main/java/com/andretaveira/dsdeliver/dto/OrderDTO.java
+++ backend/src/main/java/com/andretaveira/dsdeliver/dto/OrderDTO.java
@@ -18,6 +18,7 @@ public class OrderDTO implements Serializable {
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
+	private Double total;
 
 	private List<ProductDTO> products = new ArrayList<>();
 
@@ -25,7 +26,7 @@ public class OrderDTO implements Serializable {
 
 	}
 
-	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status, Double total) {
 		super();
 		this.id = id;
 		this.address = address;
@@ -33,6 +34,7 @@ public class OrderDTO implements Serializable {
 		this.longitude = longitude;
 		this.moment = moment;
 		this.status = status;
+		this.total = total;
 	}
 
 	public OrderDTO(Order entity) {
@@ -42,6 +44,7 @@ public class OrderDTO implements Serializable {
 		longitude = entity.getLongitude();
 		moment = entity.getMoment();
 		status = entity.getStatus();
+		total = entity.getTotal();
 		products = entity.getProducts().stream().map(p -> new ProductDTO(p)).collect(Collectors.toList());
 	}
 
@@ -87,6 +90,14 @@ public class OrderDTO implements Serializable {
 
 	public OrderStatus getStatus() {
 		return status;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
 	}
 
 	public void setStatus(OrderStatus status) {
--- backend/src/main/java/com/andretaveira/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/andretaveira/dsdeliver/entities/Order.java
@@ -31,9 +31,7 @@ public class Order implements Serializable {
 	private OrderStatus status;
 
 	@ManyToMany
-	@JoinTable(name = "tb_order_product", 
-		joinColumns = @JoinColumn(name = "order_id"), 
-		inverseJoinColumns = @JoinColumn(name = "product_id"))
+	@JoinTable(name = "tb_order_product", joinColumns = @JoinColumn(name = "order_id"), inverseJoinColumns = @JoinColumn(name = "product_id"))
 	private Set<Product> products = new HashSet<>();
 
 	public Order() {
@@ -96,6 +94,14 @@ public class Order implements Serializable {
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
+	}
+
+	public Double getTotal() {
+		double sum = 0.0;
+		for (Product p : products) {
+			sum += p.getPrice();
+		}
+		return sum;
 	}
 
 	public Set<Product> getProducts() {
